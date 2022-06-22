ef657eea950d845261dd33f46e3c69af52475916
AVANI-GAJJAR <78599804+AVANI-GAJJAR@users.noreply.github.com>
2022-06-22 18:36:27 +0530

    first commit

---
+++ GDPR_management_portal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GdprManagementPortalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'GDPR_management_portal'
---
+++ GDPR_management_portal/urls.py
@@ -0,0 +1,8 @@
+from django import views
+from django.contrib import admin
+from django.urls import path
+from GDPR_management_portal import views
+urlpatterns = [
+    path('', views.index,name="index"),
+    path('pocresult',views.poc_result,name="poc_result")
+    ]
