b63cee12099e04bfe1a2bb4c035856f5bdfa7a2a
AVANI-GAJJAR <78599804+AVANI-GAJJAR@users.noreply.github.com>
2022-06-23 21:23:59 +0530

    new files are created login module is added data extracted from csv to webpage and also compilance details

--- GDPR_management_portal/urls.py
+++ GDPR_management_portal/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from GDPR_management_portal import views
 
 urlpatterns = [
-    path('', views.index,name="index"),
+    path('',views.login,name="login"),
+    path('auth',views.auth,name="auth"),
     path('pocresult',views.poc_result,name="poc_result"),
     path('overview',views.overview,name="overview")
     ]
--- GDPR_management_portal/views.py
+++ GDPR_management_portal/views.py
@@ -1,19 +1,67 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import csv
+import pandas
+import re
 # import main as pipe
 # Create your views here.
-def index(request):
-    # nc,nonc=pipe.main_pipeline('D:\Infoware\DBI\TEST','D:\Infoware\DBI\Output')
-    nc=3
-    nonc=0
-    #nc - > number of complinet
-    #nonc - > number of non complinet
-    params={'Nos':nc,'NosNonComliant':nonc}
-    
-    return render(request,'index.html',params)
+def login(request):
+    return render(request,'login.html')
+def auth(request):
+    if request.method== "POST":
+        nm=request.POST.get('username')
+        pwd=request.POST.get('password')
+        if nm=="ddx1" and pwd=="Demo":
+            nc=3
+            nonc=0
+            #nc - > number of complinet
+            #nonc - > number of non complinet
+            params={'Nos':nc,'NosNonComliant':nonc}
+            
+            return render(request,'index.html',params)
 
 
 def poc_result(request):
-    return render(request,'poc_result.html')
+    param=[]
+    
+    file = pandas.read_csv(r'Output/Head of In-House Development and QA, Connected Car - Candidate Brief (1).csv')
+    #pandas.set_option('')
+    html_string='''
+    <html>
+        <head><title>PoC Result</tiltle></head>
+        <body>
+            
+        </body>
+    </html>
+    '''
+    file.to_html("templates/result.html",index=False)
+    with open(r"templates/result.html", 'r+') as fp:
+        # read an store all lines into list
+        lines = fp.readlines()
+        # move file pointer to the beginning of a file
+        fp.seek(0)
+        # truncate the file
+        fp.truncate()
+
+        # start writing lines except the first line
+        # lines[1:] from line 2 to last line
+        fp.writelines(lines[1:])
+        fp.close()
+    with open(r"templates/result.html", 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write("""{% extends 'base.html' %}{% block title %}PoC Test Result{% endblock title %}{% block body %} <div style='background-color:#2E9FF7'><font color='white'><h2><caption>Stack Infrastructure PoC Test Result</caption></h2></font></div>
+
+<h1>MAKE A LIST</h1><table class='table table-striped table-hover'>""" + '\n' + content+"{% endblock body %}")
+    
+    #with open("templates/result.html",'w') as f:
+    #    f.write(html_string.format())
+    """ with open(r'Output/Head of In-House Development and QA, Connected Car - Candidate Brief (1).csv') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            param.append(row) """
+        
+    return render(request,'result.html')
 def overview(request):
     return render(request,'overview.html')
