db50d3865c0855aae45b450684408416cf9c5ed7
AVANI-GAJJAR <78599804+AVANI-GAJJAR@users.noreply.github.com>
2022-06-24 13:10:53 +0530

    Required changes made

--- GDPR_management_portal/views.py
+++ GDPR_management_portal/views.py
@@ -1,4 +1,4 @@
-from turtle import ht
+#from turtle import ht
 from django.shortcuts import render
 from django.http import HttpResponse
 import csv
@@ -7,7 +7,7 @@ import re
 import pathlib
 import csv
 
-import main as pipe
+#import main as pipe
 # Create your views here.
 def login(request):
     return render(request,'login.html')
@@ -16,10 +16,12 @@ def auth(request):
         nm=request.POST.get('username')
         pwd=request.POST.get('password')
         if nm=="ddx1" and pwd=="Demo":
-            nc,nonc=pipe.main_pipeline(r'D:\Infoware\DBI\TEST',r'D:\Infoware\DBI\Output')
+           # nc,nonc=pipe.main_pipeline(r'D:\Infoware\DBI\TEST',r'D:\Infoware\DBI\Output')
+            nc=0
+            nonc=3
             params={'Nos':nc,'NosNonComliant':nonc}
             
-            return render(request,'index.html',params)
+            return render(request,r'index copy.html',params)
 
 
 def poc_result(request):
@@ -74,7 +76,7 @@ def non_complaint_file_name(request):
         df = pd.read_csv(path)
         if df['Non Compliance Elemets'][0] > 0 :
             non_complaint_file_name.append(path.name)
-            q=r'D:\Infoware\DBI\templates\\'
+            q=r'C:\Users\avani\Desktop\DBI-master\DBI\templates\\'
             q=q[:-1]
             file = pd.read_csv(path)
             temp=path.name
@@ -87,8 +89,30 @@ def non_complaint_file_name(request):
 
             s=s[:-4]+'.html'
             c=q+s
-            file.to_html(c) 
+            file.fillna('',inplace=True)
+            file= file.str.pad(20, side ='both', fillchar ='+')
+            file.to_html(c,index=False) 
             print(c)
+            
+            with open(c, 'r+') as fp:
+                # read an store all lines into list
+                lines = fp.readlines()
+                # move file pointer to the beginning of a file
+                fp.seek(0)
+                # truncate the file
+                fp.truncate()
+
+                # start writing lines except the first line
+                # lines[1:] from line 2 to last line
+                fp.writelines(lines[1:])
+                fp.close()
+            with open(c, 'r+') as f:
+                content = f.read()
+                f.seek(0, 0)
+                f.write("""{% extends 'base.html' %}{% block title %}PoC Test Result{% endblock title %}{% block body %} <div style='background-color:#2E9FF7'><font color='white'><h2><caption>Stack Infrastructure PoC Test Result</caption></h2></font></div>
+
+        <h1>MAKE A LIST</h1><table class='table table-striped table-hover'>""" + '\n' + content+"{% endblock body %}")
+            
             html_fl.append(c)
 
     mylist=zip(non_complaint_file_name,html_fl)
@@ -105,7 +129,7 @@ def complaint_file_name(request):
         df = pd.read_csv(path)
         if df['Non Compliance Elemets'][0] == 0 :
             complaint_file_name.append(path.name)
-            q=r'D:\Infoware\DBI\templates\\'
+            q=r'C:\Users\avani\Desktop\DBI-master\DBI\templates\\'
             q=q[:-1]
             file = pd.read_csv(path)
             temp=path.name
@@ -118,8 +142,30 @@ def complaint_file_name(request):
 
             s=s[:-4]+'.html'
             c=q+s
-            file.to_html(c) 
+            file.fillna('',inplace=True)
+            file= file.str.pad(20, side ='both', fillchar ='+')
+            file.to_html(c,index=False) 
+            
             html_fl.append(c)
+            with open(c, 'r+') as fp:
+                # read an store all lines into list
+                lines = fp.readlines()
+                # move file pointer to the beginning of a file
+                fp.seek(0)
+                # truncate the file
+                fp.truncate()
+
+                # start writing lines except the first line
+                # lines[1:] from line 2 to last line
+                fp.writelines(lines[1:])
+                fp.close()
+            with open(c, 'r+') as f:
+                content = f.read()
+                f.seek(0, 0)
+                f.write("""{% extends 'base.html' %}{% block title %}PoC Test Result{% endblock title %}{% block body %} <div style='background-color:#2E9FF7'><font color='white'><h2><caption>Stack Infrastructure PoC Test Result</caption></h2></font></div>
+
+        <h1>MAKE A LIST</h1><table class='table table-striped table-hover'>""" + '\n' + content+"{% endblock body %}")
+            
     mylist=zip(complaint_file_name,html_fl)
     params={"Comp":mylist}
     return render(request, 'complaint_files.html',params)
